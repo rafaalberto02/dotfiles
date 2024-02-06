1c8af0a6a3d29c6e8653d68e7d06627cfc74f6d9
rafaalberto02 <TR034850@MinhaTI.com.br>
2024-02-06 19:47:42 -0300

    modified:   after/plugin/gitsigns.lua modified:   lua/rahenriques/config.lua modified:   lua/rahenriques/remap.lua

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -9,17 +9,17 @@ require('gitsigns').setup {
 	},
 	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
 	numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
+	linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		follow_files = true
 	},
 	attach_to_untracked = true,
-	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
+		delay = 500,
 		ignore_whitespace = false,
 		virt_text_priority = 100,
 	},
@@ -27,7 +27,7 @@ require('gitsigns').setup {
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
-	max_file_length = 40000, -- Disable if file is longer than this (in lines)
+	max_file_length = 4000, -- Disable if file is longer than this (in lines)
 	preview_config = {
 		-- Options passed to nvim_open_win
 		border = 'single',
--- .config/nvim/lua/rahenriques/config.lua
+++ .config/nvim/lua/rahenriques/config.lua
@@ -1,3 +1,5 @@
+vim.o.guicursor = ""
+
 vim.o.hlsearch = true
 vim.o.ignorecase = true
 vim.o.incsearch = true
@@ -6,9 +8,28 @@ vim.o.relativenumber = true
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
+vim.o.wrap = false
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
+
+vim.o.incsearch = true
+vim.o.hlsearch = true
+
+vim.o.termguicolors = true
+
+vim.o.scrolloff = 10
+vim.o.signcolumn = "yes"
 
 vim.o.encoding = "utf-8"
-vim.o.updatetime = 300
+vim.o.updatetime = 50
+
+vim.o.colorcolumn = "80"
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
