38f7664afaf549b6e07c4b81cc37ff0c0935e3d5
rafaalberto02 <rafa.alberto02@gmail.com>
2025-04-06 00:29:47 -0300

    update nvim config

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		opts = {},
-		config = function()
-			local fzf_lua = require("fzf-lua")
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        event = "VeryLazy",
+        opts = {},
+        config = function()
+            local fzf_lua = require("fzf-lua")
 
-			fzf_lua.setup({
-				fzf_colors = true,
-			})
+            fzf_lua.setup({
+                fzf_colors = true,
+            })
 
-			vim.keymap.set("n", "<leader>fb", fzf_lua.buffers, {})
-			vim.keymap.set("n", "<leader>fbi", fzf_lua.builtin, {})
-			vim.keymap.set("n", "<leader>ff", fzf_lua.files, {})
-			vim.keymap.set("n", "<leader>fg", fzf_lua.git_files, {})
-			vim.keymap.set("n", "<leader>gg", fzf_lua.live_grep_glob, {})
-			vim.keymap.set("n", "<leader>g", fzf_lua.grep_project, {})
-			vim.keymap.set("n", "<F1>", fzf_lua.help_tags, {})
-		end,
-	},
+            vim.keymap.set("n", "<leader>fb", fzf_lua.buffers, {})
+            vim.keymap.set("n", "<leader>fin", fzf_lua.builtin, {})
+            vim.keymap.set("n", "<leader>ff", fzf_lua.files, {})
+            vim.keymap.set("n", "<leader>fg", fzf_lua.git_files, {})
+            vim.keymap.set("n", "<leader>gg", fzf_lua.live_grep_glob, {})
+            vim.keymap.set("n", "<leader>g", fzf_lua.grep_project, {})
+            vim.keymap.set("n", "<F1>", fzf_lua.help_tags, {})
+        end,
+    },
 }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,86 +1,86 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{ "saghen/blink.cmp" },
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-	},
-	cmd = { "LspInfo", "LspInstall", "LspStart" },
-	event = { "BufReadPre", "BufNewFile" },
-	init = function()
-		vim.opt.signcolumn = "yes"
-	end,
-	config = function()
-		local lsp = require("lspconfig")
-		local blink = require("blink.cmp")
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        { "saghen/blink.cmp" },
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
+        { "stevearc/conform.nvim" },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
+    },
+    cmd = { "LspInfo", "LspInstall", "LspStart" },
+    event = { "BufReadPre", "BufNewFile" },
+    init = function()
+        vim.opt.signcolumn = "yes"
+    end,
+    config = function()
+        local lsp = require("lspconfig")
+        local blink = require("blink.cmp")
 
-		local lsp_defaults = lsp.util.default_config
-		local vim_capabilities = vim.lsp.protocol.make_client_capabilities()
+        local lsp_defaults = lsp.util.default_config
+        local vim_capabilities = vim.lsp.protocol.make_client_capabilities()
 
-		local capabilities = vim.tbl_deep_extend("force", lsp_defaults.capabilities, vim_capabilities)
+        local capabilities = vim.tbl_deep_extend("force", lsp_defaults.capabilities, vim_capabilities)
 
-		capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities({}, false))
+        capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities({}, false))
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {},
-			handlers = {
-				function(server_name)
-					lsp[server_name].setup({})
-				end,
-				["lua_ls"] = function()
-					lsp.lua_ls.setup({
-						settings = {
-							Lua = {
-								runtime = {
-									version = "LuaJIT",
-									path = vim.split(package.path, ";"),
-								},
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = { vim.env.VIMRUNTIME },
-									checkThirdParty = false,
-								},
-								telemetry = {
-									enable = false,
-								},
-							},
-						},
-					})
-				end,
-				["jsonls"] = function()
-					lsp.jsonls.setup({
-						settings = {
-							json = {
-								schemas = {
-									{
-										fileMatch = { "package.json" },
-										url = "https://json.schemastore.org/package.json",
-									},
-								},
-							},
-						},
-					})
-				end,
-			},
-		})
+        require("mason-lspconfig").setup({
+            automatic_installation = true,
+            ensure_installed = {},
+            handlers = {
+                function(server_name)
+                    lsp[server_name].setup({ capabilities = capabilities })
+                end,
+                ["jsonls"] = function()
+                    lsp.jsonls.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            json = {
+                                schemas = {
+                                    {
+                                        fileMatch = { "package.json" },
+                                        url = "https://json.schemastore.org/package.json",
+                                    },
+                                },
+                            },
+                        },
+                    })
+                end,
+            },
+        })
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			desc = "LSP actions",
-			callback = function(event)
-				local opts = { buffer = event.buf }
+        vim.api.nvim_create_autocmd("LspAttach", {
+            desc = "LSP actions",
+            callback = function(event)
+                local opts = { buffer = event.buf }
 
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			end,
-		})
-	end,
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+                vim.keymap.set("n", "<leader>f", function()
+                    if client and client.supports_method("textDocument/formatting") then
+                        vim.lsp.buf.format({ bufnr = event.buf, id = client.id })
+                    else
+                        require("conform").format({ async = true })
+                    end
+                end, opts)
+            end,
+        })
+    end,
 }
