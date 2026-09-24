e83e714bcb20a8731d4311466864d4ceeb313dae
Rafael Henriques <rafael.henriques@localiza.com>
2026-09-24 09:45:28 -0300

    add vscode keybindings

--- .config/nvim/lua/vscode-nvim/config.lua
+++ .config/nvim/lua/vscode-nvim/config.lua
@@ -4,6 +4,8 @@ local opts = { silent = true }
 
 local vscode = require("vscode")
 
+vim.notify = vscode.notify
+
 vim.keymap.set("n", "K", function() vscode.action("editor.action.showHover") end, opts)
 vim.keymap.set("n", "gd", function() vscode.action("editor.action.revealDefinition") end, opts)
 vim.keymap.set("n", "gD", function() vscode.action("editor.action.revealDeclaration") end, opts)
@@ -13,3 +15,18 @@ vim.keymap.set("n", "gr", function() vscode.action("references-view.findReferenc
 vim.keymap.set("n", "gs", function() vscode.action("editor.action.triggerParameterHints") end, opts)
 vim.keymap.set("n", "<leader>rn", function() vscode.action("editor.action.rename") end, opts)
 vim.keymap.set("n", "<leader>ca", function() vscode.action("editor.action.codeAction") end, opts)
+
+vim.keymap.set("n", "<leader>f", function() vscode.action("editor.action.formatDocument") end, opts)
+
+vim.keymap.set("n", "<leader>ff", function() vscode.action("workbench.action.quickOpen") end, opts)
+vim.keymap.set("n", "<leader>fg", function() vscode.action("workbench.action.findInFiles") end, opts)
+
+vim.keymap.set("n", "<leader>xx", function() vscode.action("workbench.actions.view.problems") end, opts)
+vim.keymap.set("n", "<leader>st", function() vscode.action("workbench.action.terminal.new") end, opts)
+
+vim.keymap.set("n", "<leader>pv", function() vscode.action("workbench.view.explorer") end, opts)
+
+vim.keymap.set("n", "<C-o>", function() vscode.action("workbench.action.navigateBack") end, opts)
+vim.keymap.set("n", "<C-i>", function() vscode.action("workbench.action.navigateForward") end, opts)
+
+
